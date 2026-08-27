d54d56ed3651cd3dbd06b28a0bbdaf47937f350a
rnavya1017 <ravagunta.navya@gmail.com>
2026-08-27 14:27:48 +0530

    [Navya] : Add Employee repository with duplicate validation queries

---
+++ src/main/java/com/bridgelabz/EmployeePayrollApplication/repository/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.EmployeePayrollApplication.repository;
+
+import com.bridgelabz.EmployeePayrollApplication.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
+}
